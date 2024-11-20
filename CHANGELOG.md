00ba59b301dbaa4873521ada2c7400d960924a49
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2024-11-20 16:01:55 +1300

    Fixed serde deserialization of signed ints for `Model`. Added toml tests.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# Version 0.1.1 - 20-11-2024
+- **Add**: Added test for toml support
+- **Fix**: Fixed serde deserialization of signed ints for `Model`
 # Version 0.1.0 - 20-11-2024
 - Initial release of the library
 - Support for XM430, XM540, XC330, YM070
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -130,6 +130,10 @@ impl<'de> Deserialize<'de> for Model {
                 Model::from_u64(value)
                     .ok_or_else(|| E::invalid_value(de::Unexpected::Unsigned(value), &self))
             }
+            fn visit_i64<E: de::Error>(self, value: i64) -> Result<Self::Value, E> {
+                Model::from_i64(value)
+                    .ok_or_else(|| E::invalid_value(de::Unexpected::Signed(value), &self))
+            }
 
             fn visit_str<E: de::Error>(self, value: &str) -> Result<Self::Value, E> {
                 Model::from_str(value)
@@ -180,12 +184,34 @@ mod tests {
 
     #[test]
     #[cfg(feature = "serde")]
-    fn test_serde() {
+    fn test_serde_json() {
         let model = Model::XM430_W210;
         let json = serde_json::to_string(&model).unwrap();
         assert_eq!(json, "1030");
 
         let model: Model = serde_json::from_str("\"XM430_W350\"").unwrap();
-        assert_eq!(model, Model::XM430_W210);
+        assert_eq!(model, Model::XM430_W350);
+
+        let model: Model = serde_json::from_str("1020").unwrap();
+        assert_eq!(model, Model::XM430_W350);
+    }
+    #[test]
+    #[cfg(feature = "serde")]
+    fn test_serde_toml() {
+        #[derive(Deserialize, serde::Serialize)]
+        struct Test {
+            model: Model
+        }
+        let model = Test {
+            model: Model::XM430_W210
+        };
+        let toml = toml::to_string(&model).unwrap();
+        assert_eq!(toml, "model = 1030\n");
+
+        let model: Test = toml::from_str("model = \"XM430_W350\"").unwrap();
+        assert_eq!(model.model, Model::XM430_W350);
+
+        let model: Test =toml::from_str("model = 1020").unwrap();
+        assert_eq!(model.model, Model::XM430_W350);
     }
 }
