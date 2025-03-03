2dfb0899fa6268cb928558f194300b3f70025f1b
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2025-03-04 11:43:10 +1300

    use regex to match model with ct

--- src/control_table.rs
+++ src/control_table.rs
@@ -5,6 +5,7 @@
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use regex::Regex;
 use crate::models::{Error, Model, XM430, YM};
 use crate::register::Register;
 use crate::RegisterData;
@@ -23,18 +24,17 @@ pub struct ControlTable {
 impl ControlTable {
     /// Create a new control table for a specific model. If the model is not yet implemented, the error [`Error::NotImplemented`] is returned.
     pub fn new(model: Model) -> Result<Self, Error> {
-        let table = match model {
-            Model::XM430_W210 | Model::XM430_W350 => XM430::table(),
-            Model::XM540_W150 | Model::XM540_W270 => XM430::table(),
-            Model::XC330_M181
-            | Model::XC330_M288
-            | Model::XC330_T181
-            | Model::XC330_T288 => XM430::table(),
-            Model::YM070_200_R051_R
-            | Model::YM070_200_R099_R
-            | Model::YM070_200_A099_R
-            | Model::YM080_230_R099_R => YM::table(),
-            _ => return Err(Error::NotImplemented),
+        let table = {
+            let model = model.to_string();
+            if Regex::new(r"X\w430|540").unwrap().is_match(&model) {
+                XM430::table()
+            } else if Regex::new(r"X\w330").unwrap().is_match(&model) {
+                XM430::table()
+            } else if Regex::new(r"YM.*").unwrap().is_match(&model) {
+                YM::table()
+            } else {
+                return Err(Error::NotImplemented);
+            }
         };
         Ok(ControlTable {
             model,
@@ -57,5 +57,43 @@ impl TryFrom<Model> for ControlTable {
     type Error = Error;
     fn try_from(model: Model) -> Result<Self, Self::Error> {
         ControlTable::new(model)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn xm430() {
+        let models = [
+            Model::XM430_W210,
+            Model::XM430_W350,
+            Model::XM540_W150,
+            Model::XM540_W270,
+            Model::XC330_M181,
+            Model::XC330_M288,
+            Model::XC330_T181,
+            Model::XC330_T288,
+            Model::XH430_V210,
+        ];
+        for model in models {
+            let table = ControlTable::new(model).unwrap();
+            assert_eq!(table.table, XM430::table());
+        }
+    }
+
+    #[test]
+    fn ym() {
+        let models = [
+            Model::YM070_200_R051_R,
+            Model::YM070_200_R099_R,
+            Model::YM070_200_A099_R,
+            Model::YM080_230_R099_R,
+        ];
+
+        for model in models {
+            let table = ControlTable::new(model).unwrap();
+            assert_eq!(table.table, YM::table());
+        }
     }
 }
