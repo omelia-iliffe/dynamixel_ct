6f9f2ae84fab2477f4d9770f6e01c89b7d3d086d
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2025-03-06 16:22:23 +1300

    add support for multiple models in macro

--- src/model_macro.rs
+++ src/model_macro.rs
@@ -1,11 +1,10 @@
 //! The [`crate::model!`] macro definition.
 //!
+
 /// The [`model!`] macro is used to define the control table for a specific model.
 /// It creates a static HashMap of RegisterData for each register in the model.
-
 macro_rules! model {
-    ($model:ident { $($reg:ident : $addr:expr, $len:expr,)+ } ) => {
-        #[doc = concat!("The control table for the ", stringify!($model), " model.")]
+    ($($model:ident)+ { $($reg:ident : $addr:expr, $len:expr,)+ } ) => {
         pub(crate) static TABLE: std::sync::LazyLock<std::collections::HashMap<$crate::register::Register, $crate::register::RegisterData>> = std::sync::LazyLock::new(|| {
             [
                 $(
@@ -17,32 +16,34 @@ macro_rules! model {
             ].iter().cloned().collect()
         });
 
-        #[doc = concat!("The Control Table for the ", stringify!($model), " models.")]
-        pub struct $model(&'static std::collections::HashMap<$crate::register::Register, $crate::register::RegisterData>);
+        $(
+            #[doc = concat!("The Control Table for the ", stringify!($model), " models.")]
+            pub struct $model(&'static std::collections::HashMap<$crate::register::Register, $crate::register::RegisterData>);
 
-        impl $model {
-            #[doc = concat!("Constructs the control table for the ", stringify!($model), " models.")]
-            #[doc = concat!("The control table is statically allocated to reduce memory usage.")]
-            pub fn new() -> Self {
-                Self(&*TABLE)
+            impl $model {
+                #[doc = concat!("Constructs the control table for the ", stringify!($model), " models.")]
+                #[doc = concat!("The control table is statically allocated to reduce memory usage.")]
+                pub fn new() -> Self {
+                    Self(&*TABLE)
+                }
+
+                /// Get the register data for a specific register.
+                pub fn get(&self, register: $crate::register::Register) -> Option<&$crate::RegisterData> {
+                    self.0.get(&register)
+                }
+
+                #[doc = concat!("Acquire a static reference to the control table for the ", stringify!($model), " models.")]
+                pub(crate) fn table() -> &'static std::collections::HashMap<$crate::register::Register, $crate::register::RegisterData> {
+                    &*TABLE
+                }
             }
 
-            /// Get the register data for a specific register.
-            pub fn get(&self, register: $crate::register::Register) -> Option<&$crate::RegisterData> {
-                self.0.get(&register)
+            impl Default for $model {
+                fn default() -> Self {
+                    Self::new()
+                }
             }
-
-            #[doc = concat!("Acquire a static reference to the control table for the ", stringify!($model), " models.")]
-            pub(crate) fn table() -> &'static std::collections::HashMap<$crate::register::Register, $crate::register::RegisterData> {
-                &*TABLE
-            }
-        }
-
-        impl Default for $model {
-            fn default() -> Self {
-                Self::new()
-            }
-        }
+        )+
     }
 }
 
